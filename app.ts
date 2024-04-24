76242d2bbb568ac490e463c47a927bbb42971394
chadcito <145295470+chadcito@users.noreply.github.com>
2024-04-24 15:30:05 -0600

    set handlebars as the view engine and rendered primary layout

--- app.ts
+++ app.ts
@@ -1,7 +1,26 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+
+import { engine } from 'express-handlebars';
 
 const app = express();
 const port: number = 3000;
+
+app.set('view engine', 'hbs');
+
+app.engine(
+  'hbs',
+  engine({
+    layoutsDir: `${__dirname}/views/layouts`,
+    extname: 'hbs',
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  console.log('landing hit');
+  res.render('main', { layout: 'index' });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
